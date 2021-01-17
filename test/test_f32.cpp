fe5f3d04723799ca7fdee554bbe150d4791b5d35
Yanjie He <heyanjie0@outlook.com>
2021-01-17 00:00:24 -0500

    add hyphen between "floating" and "point"

--- test/test_f32.cpp
+++ test/test_f32.cpp
@@ -4,7 +4,7 @@
 using std::vector;
 
 TEST_CASE("test f32 to bytes", "[f32]") {
-  SECTION("floating point numbers") {
+  SECTION("floating-point numbers") {
     REQUIRE(bit_converter::f32_to_bytes(3.14, false) ==
             vector<uint8_t>{195, 245, 72, 64});
   }
--- test/test_f64.cpp
+++ test/test_f64.cpp
@@ -4,7 +4,7 @@
 using std::vector;
 
 TEST_CASE("test f64 to bytes", "[f64]") {
-  SECTION("floating point numbers") {
+  SECTION("floating-point numbers") {
     REQUIRE(bit_converter::f64_to_bytes(3.14, false) ==
             vector<uint8_t>{31, 133, 235, 81, 184, 30, 9, 64});
   }
